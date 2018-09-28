b013937d7f8fc07ec98efe2cb85c171426dac57f
sondos alansi <sondos.alansi@yahoo.com>
2018-09-28 20:55:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers=function(drivers){
+  
+  return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers=function(drivers){
+  return drivers.slice(-2)
+}
+const selectingDrivers=[returnFirstTwoDrivers
+ , 
+returnLastTwoDrivers
+];
+
+function  createFareMultiplier(num){
+  
+  return function(fare){
+    return fare*fare;
+  }
+}
+  
+const fareDoubler=function  createFareMultiplier(fare){
+    return fare*2;
+}
+
+const fareTripler=function  createFareMultiplier(fare){
+    return fare*3;
+}
+function selectDifferentDrivers(drivers,returnLastTwoDrivers){
+  
+  return returnLastTwoDrivers(drivers);
+}
+ 
+
+  
+  
+ 
